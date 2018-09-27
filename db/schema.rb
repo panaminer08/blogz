4b7bbf9432d346b266bc7e9987cb051005f1e931
panaminer08 <adamsrey@outlook.com>
2018-09-27 10:07:01 -0400

    renamed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_224415) do
+ActiveRecord::Schema.define(version: 2018_09_27_135909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
